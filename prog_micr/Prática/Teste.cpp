73126a9202be800ae770183320f7d3450b3101e0
GuiSilva77 <79546633+GuiSilva77@users.noreply.github.com>
2021-03-04 19:44:50 -0300

    Alterações no código e pratica dos conhecimentos

---
+++ prog_micr/Prática/Teste.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    int a, b, c, d;
+
+    a = 0;
+    b = 0;
+    c = 0;
+    d = 0;
+
+    cin
+ >> a;
+
+
+
+
+
+
+}
