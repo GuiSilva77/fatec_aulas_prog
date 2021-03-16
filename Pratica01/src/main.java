65a440e84eabdcace93033c0775c06877ae5bb85
GuiSilva77 <79546633+GuiSilva77@users.noreply.github.com>
2021-03-16 18:58:57 -0300

    Alteração no código 16/03/21

---
+++ Pratica01/src/main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n1;
+		
+		System.out.print("Digite um número: ");
+		scanner.nextInt(n1);
+		
+		System.out.print("O número é " + n1 + ".");
+	}
+
+}
