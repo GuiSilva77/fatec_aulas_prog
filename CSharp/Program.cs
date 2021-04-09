7e40ca62a85805b36a3c5aa4bc7e23aa1a6a85fc
GuiSilva77 <79546633+GuiSilva77@users.noreply.github.com>
2021-04-09 16:19:04 -0300

    Teste c#

---
+++ CSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (2 < 0)
+            {
+            Console.WriteLine("Goodbye!");
+            }
+            else
+                {
+                Console.WriteLine("Hello!");
+                }
+        }
+    }
+}
--- Pratica01/testenode01.js
+++
@@ -1,3 +0,0 @@
-var escola = [1, 7, 4, 6];
-
-console.log(escola.length);
