151046f7f1673678444d1c9e4a76e6283f6b55ac
GuiSilva77 <79546633+GuiSilva77@users.noreply.github.com>
2021-04-12 20:32:20 -0300

    Alteração de código

---
+++ ex01.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+
+void soma(int num1, int num2)
+{
+    int soma = num1 + num2;
+}
+
+int main()
+{
+    int num1, num2;
+    std::cin >> num1;
+
+    std::cin >> num2;
+
+    int soma = num1 + num2;
+
+
+    std::cout << soma;
+
+    return 0;
+}
