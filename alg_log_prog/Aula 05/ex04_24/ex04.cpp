db36048a986509ec4f41d04096226f9404e3b656
GuiSilva77 <79546633+GuiSilva77@users.noreply.github.com>
2021-04-16 22:40:08 -0300

    Resolução do exercício 04

--- alg_log_prog/Aula 05/ex04_24/ex04.cpp
+++ alg_log_prog/Aula 05/ex04_24/ex04.cpp
@@ -13,17 +13,51 @@ int main()
     switch (opcao)
     {
     case 1:
+      float salario, imposto;
+      printf("\nDigite o seu salario: R$ ");
+      scanf("%f%*c", &salario);
 
+      if (salario < 500)
+        imposto = 5;
+      else if (salario <= 850)
+        imposto = 10;
+           else
+             imposto = 15;
+
+        printf("Imposto: %.0f porcento.", imposto);
     break;
     case 2:
+      float novo_salario;
+      printf("\nDigite o seu salario: R$ ");
+      scanf("%f%*c", &salario);
 
+      if (salario < 450)
+        novo_salario = salario + 100;
+      else if (salario < 750)
+        novo_salario = salario + 75;
+           else if (salario <= 1500)
+             novo_salario = salario + 50;
+                else
+                  novo_salario = salario + 25;
+
+        printf("Seu novo salario sera de: R$ %.2f", novo_salario);
     break;
     case 3:
+      printf("\nDigite o seu salario: R$ ");
+      scanf("%f%*c", &salario);
 
+      if (salario <= 700)
+        printf("Classificacao: Mal remunerado.");
+      else
+        printf("Classificacao: Bem remunerado.");
     break;
     case 4:
         return 0;
     break;
+    default:
+      printf("insira uma opcao valida.");
+    break;
     }
+    getchar();
     return 0;
 }
