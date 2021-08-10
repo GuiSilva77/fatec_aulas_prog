1781e23d52ffc4ee04ef9adc0987b35a89730690
GuiSilva77 <guilherme92325@gmail.com>
2021-08-10 19:57:04 -0300

    Prática e Aula 01 de PS

---
+++ Prática/C  C++/pr01/pr01.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char** argv) {
+  
+  printf("Hello World");
+
+  system("Pause");
+    return EXIT_SUCCESS;
+}
--- Prática/HTML CSS/pr01/codex.js
+++ Prática/HTML CSS/pr01/codex.js
@@ -1,1 +1,21 @@
-result.innerHTML = `<p>Hello World!</p>`;
+class Aluno{ // criação da classe aluno
+  constructor(nome, idade, turma, media){
+    this.nome = nome
+    this.idade = idade
+    this.turma = turma
+    this.media = media
+  }
+
+  getAluno(){console.log(classe)} // funcção da chamada de aluno
+}
+
+let classe = []
+form.addEventListener("submit", (event) =>{
+  event.preventDefault();
+  i = 0
+
+
+  i++
+
+  console.log(classe)
+})
